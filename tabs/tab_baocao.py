05d9490d46215c9745d6a7dfc7622104345c009c
xuantruongvbsp <xuantruongvbsp@gmail.com>
2026-05-12 14:23:01 +0700

    Update tab_baocao.py

--- tabs/tab_baocao.py
+++ tabs/tab_baocao.py
@@ -100,7 +100,11 @@ def render(tab: DeltaGenerator, **kwargs: dict) -> None:
     username = kwargs.get("username")
     df_nq11 = kwargs.get("df_nq11")
 
-    with tab:
+    if tab is not None:
+        _ctx = tab
+    else:
+        _ctx = st.container()
+    with _ctx:
         st.subheader("📈 Báo cáo")
 
         COL_CHUNG = [c for c in [

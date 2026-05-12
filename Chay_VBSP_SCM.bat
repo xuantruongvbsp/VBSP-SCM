20c457eff5c644f5b6a59639a6fabf0536faa28b
xuantruongvbsp <xuantruongvbsp@gmail.com>
2026-05-12 15:52:03 +0700

    Update Chay_VBSP_SCM.bat

--- Chay_VBSP_SCM.bat
+++ Chay_VBSP_SCM.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d %~dp0
 echo Dang khoi dong VBSP-SCM...
+start /b .\.venv\Scripts\streamlit run app.py
+timeout /t 4 /nobreak >nul
 start "" http://localhost:8501
-.\.venv\Scripts\streamlit run app.py
+echo Server dang chay tai http://localhost:8501
 pause

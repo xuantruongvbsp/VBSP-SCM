db25d24530db6af1c4e44c216e7cf38173323884
xuantruongvbsp <xuantruongvbsp@gmail.com>
2026-05-21 05:54:53 +0700

    Update __init__.py

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -1,8 +1,1 @@
-from tabs import (
-    tab_tongquan, tab_tracuu, tab_danhsach, tab_baocao,
-    tab_candoi, tab_kehoach, tab_cbtd,
-    tab_gqvl, tab_khtd, tab_khtd_pgd,
-    tab_nhiem_vu, tab_upload_khnv, tab_upload_pgd,
-    tab_cdtotkvv, tab_cdtotkvv_pgd, tab_khtd_mau07, tab_khtd_giao_dc,
-    tab_tien_do,
-)
+"""Tất cả tab modules được lazy import qua importlib.import_module() từ workspaces."""
